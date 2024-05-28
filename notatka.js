ac51c614fcefc42b8af9336412393c757cac92ce
cojak <113033656+1pjastrzebski@users.noreply.github.com>
2024-05-28 12:03:34 +0200

    Add files via upload

--- notatka.js
+++ notatka.js
@@ -1,9 +1,31 @@
 const notes = document.querySelectorAll(".note")
-console.log(notes)
+const main = document.querySelector(".container")
+const addBtn = document.querySelector(".addBtn")
 notes.forEach((note)=>{
     note.addEventListener('click',()=>{
-        note.classList.replace("note","bigNote") 
-        
+        if(note.classList.contains("note")){
+            note.classList.replace("note","bigNote") 
+            notes.forEach((note)=>{
+                if(note.classList.contains("note")){
+                    note.style.display = "none";
+                }
+            })
+            addBtn.style.display = "none";
+            setTimeout(()=>{
+                main.addEventListener("click",mainFunc)
+                function mainFunc(){
+                    notes.forEach((note)=>{
+                        
+                        note.style.display = "block"
+                        if(note.classList.contains("bigNote")){
+                            note.classList.replace("bigNote","note")
+                        }
+                        addBtn.style.display = "block"
+                    })
+                    main.removeEventListener("click",mainFunc)
+                }
+            },1)
+        }
 
     })
 })
